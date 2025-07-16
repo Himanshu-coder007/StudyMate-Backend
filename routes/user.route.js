d02d0d7c3aabd2966f069641d6bb632ca14912e9
Himanshu-coder007 <himanshunagose54@gmail.com>
2025-07-16 20:04:51 +0530

    auth check removed from conntroller and router

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,6 @@ import {
   register,
   login,
   logout,
-  checkAuth,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -11,6 +10,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
-router.get("/check-auth", checkAuth); // Add this new route
+
 
 export default router;
